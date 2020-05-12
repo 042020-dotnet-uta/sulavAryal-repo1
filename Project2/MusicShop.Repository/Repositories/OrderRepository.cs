4f3ca51d7322ac103cf76bcde53fadc456818982
sulav <sulav.aryal@outlook.com>
2020-05-12 13:01:39 -0700

    adds entities, and repositories.

---
+++ Project2/MusicShop.Repository/Repositories/OrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using MusicShop.Domain;
+using MusicShop.Repository.DataAccess;
+using MusicShop.Repository.IRepositories;
+
+namespace MusicShop.Repository
+{
+    public class OrderRepository : GenericRepository<Order>, IOrderRepository
+    {
+        private readonly MSDbContext _context;
+
+        public OrderRepository(MSDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ Project2/MusicShop.Repository/Repositories/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using MusicShop.Domain;
+using MusicShop.Repository.DataAccess;
+using MusicShop.Repository.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicShop.Repository
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        private readonly MSDbContext _context;
+
+        public ProductRepository(MSDbContext context):base(context)
+        {
+            _context = context;
+        }
+    }
+}
