78e0e07ef0338b5ffbac20e29cee5f53cdefdf8d
sulav <sulav.aryal@outlook.com>
2020-05-19 14:38:10 -0700

    add fridge service assignment

---
+++ FridgeService/GettingStartedClient/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GettingStartedClient.ServiceReference1;
+
+namespace GettingStartedClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step 1: Create an instance of the WCF proxy.
+            FridgeClient client = new FridgeClient();
+
+
+            var fridgeContent = client.FridgeContents();
+            Console.WriteLine("Fridge intially has these fruits in it.");
+            foreach (var item in fridgeContent)
+            {
+                Console.WriteLine(item);
+            }
+
+            int result = 0;
+            result = client.GetFruitTotal();
+            Console.WriteLine($"Total fruits in fridge is : {result}\n");
+
+
+
+            string fruit = "Papaya";
+            result = client.AddFruit("Papaya");
+            Console.WriteLine($"{fruit} added in");
+
+            fridgeContent = client.FridgeContents();
+            Console.WriteLine("Fridge now has these fruits in it.");
+            foreach (var item in fridgeContent)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"Total fruits in fridge is : {result}\n");
+
+            result = client.TakeFruit(fruit);
+            Console.WriteLine($"\nI took out the {fruit} and ate it");
+
+            fridgeContent = client.FridgeContents();
+            Console.WriteLine("Fridge now has these fruits in it.");
+            foreach (var item in fridgeContent) 
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"Total fruits in fridge is : {result}\n");
+
+
+            // Step 3: Close the client to gracefully close the connection and clean up resources.
+            Console.WriteLine("\nPress <Enter> to terminate the client.");
+            Console.ReadLine();
+            client.Close();
+        }
+    }
+}
---
+++ FridgeService/GettingStartedLib/Service1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace GettingStartedLib
+{
+    
+   
+    public class FridgeService : IFridge
+    {
+        public List<string> Fridge { get; set; } = new List<string>{ "Apple", "Banana", "Orange"};
+
+        public int GetFruitTotal()
+        {
+            int count = Fridge.Count;
+            return count;
+        }
+
+        public int AddFruit(string fruit) 
+        {
+            Fridge.Add(fruit);
+            return Fridge.Count;
+        }
+
+        public int TakeFruit(string fruit) 
+        {
+            if (Fridge.Contains(fruit)) 
+            {
+                Fridge.Remove(fruit);
+                return Fridge.Count;
+            }
+            return 0;
+        }
+
+        public List<string> FridgeContents() 
+        {
+            var fridgeContent = Fridge;
+            return fridgeContent;
+        }
+
+
+       
+    }
+}
